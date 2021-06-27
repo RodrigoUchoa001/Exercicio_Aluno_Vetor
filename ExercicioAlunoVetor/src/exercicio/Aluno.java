c8e6fd88d6ca171c96cbd705dc3569a47c4e3bb1
RodrigoUchoa001 <85903922+RodrigoUchoa001@users.noreply.github.com>
2021-06-27 18:28:53 -0300

    Adicionada os gets e sets

--- ExercicioAlunoVetor/src/exercicio/Aluno.java
+++ ExercicioAlunoVetor/src/exercicio/Aluno.java
@@ -1,5 +1,21 @@
 package exercicio;
 
 public class Aluno {
+	private String nome;
+	private double nota;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	
 
 }
