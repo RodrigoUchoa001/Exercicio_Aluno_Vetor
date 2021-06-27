9d146b5c94c5014e25d72b34b7aaed5a53e02fca
RodrigoUchoa001 <85903922+RodrigoUchoa001@users.noreply.github.com>
2021-06-27 18:46:02 -0300

    Adicionada as funções em Vetor

--- ExercicioAlunoVetor/src/exercicio/Vetor.java
+++ ExercicioAlunoVetor/src/exercicio/Vetor.java
@@ -4,6 +4,41 @@ public class Vetor {
 	private Aluno[] aluno = new Aluno[10];
 	private int cont;
 	
+	public Vetor() {
+		cont=0;
+	}
 	
-
+	public boolean adiciona(Aluno aluno) {
+		if (cont<10) {
+			this.aluno[cont]=aluno;
+			cont++;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean busca(int posicao) {
+		if (posicao>=0&&posicao<cont) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean remove(int posicao) {
+		if (posicao>=0&&posicao<cont) {
+			for (int i=posicao;i<cont-1;i++) {
+				aluno[i]=aluno[i+1];
+			}
+			aluno[cont]=null;
+			cont--;
+			return true;
+		}
+		return false;
+	}
+	
+	public void getLista() {
+		for (int i=0;i<cont;i++) {
+			System.out.println(aluno[i]);
+		}
+	}
 }
