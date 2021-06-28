6d40ff6b778a2d3167c5708b22fcf35f8c324d12
RodrigoUchoa001 <85903922+RodrigoUchoa001@users.noreply.github.com>
2021-06-27 22:15:59 -0300

    Mais testes adicionados na classe main

--- ExercicioAlunoVetor/src/exercicio/Principal.java
+++ ExercicioAlunoVetor/src/exercicio/Principal.java
@@ -6,21 +6,26 @@ public class Principal {
 		Aluno a1=new Aluno("Wollys",8.0);
 		Aluno a2=new Aluno("Pablo",7.0);
 		Aluno a3=new Aluno("fksjdflsd",2.0);
+		Aluno a4=new Aluno("teste",6.0);
 		
 		Vetor alunos=new Vetor();
 		alunos.adiciona(a1);
 		alunos.adiciona(a2);
 		alunos.adiciona(a3);
+		alunos.adiciona(a4);
 		
-		System.out.println(alunos.remove(4));
+		alunos.getLista();
+		System.out.println("\n"+alunos.remove(2));
+		alunos.getLista();
 		
-		Aluno x=alunos.getAlunoP(4);
+		System.out.println("\n"+alunos.remove(4));
+		
+		Aluno x=alunos.getAlunoP(2);
 		if (x!=null) {
-			System.out.println(x);
+			System.out.println("\n"+x);
 		}else {
 			System.out.println("Posição Inválida!");
-		}
-		
+		}	
 	}
 
 }
